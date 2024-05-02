266398e5f29315c0c4392f7861a445d176e3e141
dongyoung <dong0579@naver.com>
2024-05-02 16:28:31 +0900

    Feat: Google Social Login Prototype

---
+++ src/main/java/com/cotato/squadus/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.cotato.squadus.config.auth;
+
+import com.cotato.squadus.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(requests -> requests
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                ).logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                ).oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                .userService(customOAuth2UserService)));
+        return http.getOrBuild();
+    }
+}
