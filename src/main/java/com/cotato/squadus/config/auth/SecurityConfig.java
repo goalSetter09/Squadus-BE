12ddb54c17a17fd594136ac71685fad319ec049e
dongyoung <dong0579@naver.com>
2024-05-04 00:41:11 +0900

    Feat: Kakao Social Login 구현 - OAuthAttributes에서 ofKakao 매서드를 통해 카카오 프로필 정보 받아옴

--- src/main/java/com/cotato/squadus/config/auth/SecurityConfig.java
+++ src/main/java/com/cotato/squadus/config/auth/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(requests -> requests
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
                         .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
@@ -26,6 +27,7 @@ public class SecurityConfig {
                 ).logout(logout -> logout
                         .logoutSuccessUrl("/")
                 ).oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/")
                         .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
                                 .userService(customOAuth2UserService)));
         return http.getOrBuild();
--- src/main/java/com/cotato/squadus/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/cotato/squadus/config/auth/dto/OAuthAttributes.java
@@ -24,8 +24,22 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public  static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if (registrationId.equals("kakao")) {
+            return ofKakao(userNameAttributeName, attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");  // 카카오로 받은 데이터에서 계정 정보가 담긴 kakao_account 값을 꺼낸다.
+        Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");   // 마찬가지로 profile(nickname, image_url.. 등) 정보가 담긴 값을 꺼낸다.
+
+        return new OAuthAttributes(attributes,
+                userNameAttributeName,
+                (String) profile.get("nickname"),
+                (String) kakao_account.get("email"),
+                (String) profile.get("profile_image_url"));
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
